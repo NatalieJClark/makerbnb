a3466d65b44f957f890b22e6c21760d109dac41b
Natalie Clark <nj_clark@hotmail.co.uk>
2023-11-07 12:31:05 +0000

    Add simple LandingPage template

--- src/main/java/com/makers/makersbnb/controller/StaticPageController.java
+++ src/main/java/com/makers/makersbnb/controller/StaticPageController.java
@@ -2,12 +2,14 @@ package com.makers.makersbnb.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class StaticPageController {
+    // Home page
     @GetMapping("/")
-    public String landingPage() {
-        return "Welcome to MakersBnB!";
+    public ModelAndView welcome() {
+        return new ModelAndView("/LandingPage");
     }
 
     @GetMapping("/contactus")
