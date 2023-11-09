f0a3016673f212ea228915d89b5ed954ae1f770e
Natalie Clark <nj_clark@hotmail.co.uk>
2023-11-09 17:13:16 +0000

    Add SpaceRepository interface

--- src/main/java/com/makers/makersbnb/repository/SpaceRepository.java
+++ src/main/java/com/makers/makersbnb/repository/SpaceRepository.java
@@ -3,4 +3,5 @@ package com.makers.makersbnb.repository;
 import com.makers.makersbnb.model.Space;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SpaceRepository extends CrudRepository<Space, Long> { }
+public interface SpaceRepository extends CrudRepository<Space, Long> {
+}
