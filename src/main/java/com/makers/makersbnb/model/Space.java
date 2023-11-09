1465b5f9747b0c72bbfa8f991ccf0901d60507f0
Natalie Clark <nj_clark@hotmail.co.uk>
2023-11-09 12:14:10 +0000

    Add description and price instance fields

--- src/main/java/com/makers/makersbnb/model/Space.java
+++ src/main/java/com/makers/makersbnb/model/Space.java
@@ -4,27 +4,44 @@ import jakarta.persistence.*;
 
 // @Entity - instances of this class map to database records
 @Entity
-// @Table - those records can ve found in the spaces table
+// @Table - those records can be found in the spaces table
 @Table
 public class Space {
-    // the following field (id) is the primary key for this Entity
+    // id field is the primary key for this Entity
     @Id
     //the value of id is generated automatically
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // a second field that holds the name of each space
+    // field which holds the name of each space
     private String name;
+
+    // field which holds the description of each space
+    private String description;
+
+    // field which hold the price of each space
+    private int price;
+
 
     // a zero-arguments constructor
     public Space() {}
 
     // a one-argument constructor
-    public Space(String name) {
+    public Space(String name, String description, int price) {
         this.name = name;
+        this.description = description;
+        this.price = price;
     }
 
     // methods that get and set name
     public String getName() { return this.name; }
-    public void setname(String name) { this.name = name; }
+    public void setName(String name) { this.name = name; }
+
+    // methods that get and set description
+    public String getDescription() { return this.description; }
+    public void setDescription(String description) { this.description = description; }
+
+    // methods that get and set price
+    public int getPrice() { return this.price; }
+    public void setPrice(int price) { this.price = price; }
 }
