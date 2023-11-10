8537d2f0c856e12e7aab9f54db6cbff61b07744d
Natalie Clark <nj_clark@hotmail.co.uk>
2023-11-10 18:45:15 +0000

    Add welcome message for logged in user

--- src/main/java/com/makers/makersbnb/controller/SpacesController.java
+++ src/main/java/com/makers/makersbnb/controller/SpacesController.java
@@ -3,11 +3,15 @@ package com.makers.makersbnb.controller;
 import com.makers.makersbnb.model.Space;
 import com.makers.makersbnb.repository.SpaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Map;
 
 @RestController
 public class SpacesController {
@@ -15,15 +19,23 @@ public class SpacesController {
     @Autowired
     SpaceRepository spaceRepository;
     @GetMapping("/spaces")
-    public ModelAndView listSpaces() {
+    public ModelAndView listSpaces(@AuthenticationPrincipal OAuth2User principal) {
         ModelAndView modelAndView = new ModelAndView("spaces/index");
+
+        // getAttributes
+        Map attributes = principal.getAttributes();
+        // they will then be printed when you go to "/spaces"
+        System.out.println(attributes);
+
+        String username = attributes.get("login").toString();
+        System.out.println(username);
+        modelAndView.addObject("username", username);
 
         Iterable<Space> spaces = spaceRepository.findAll();
         modelAndView.addObject("spaces", spaces);
 
         Space space = new Space();
         modelAndView.addObject("space", space);
-
         return modelAndView;
     }
 
